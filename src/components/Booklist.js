8359c5fb2c3b74d2ea33347c93e15319c7a0ba13
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-11-09 23:25:35 +0700

    add random book

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,25 +1,36 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import uuid from "uuid/v1";
 
 const Booklist = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
-
   const [theme, setTheme] = useState({});
+  const [books, setBooks] = useState([
+    { title: "The way of kings", id: uuid() },
+    { title: "The name of the wind", id: uuid() },
+    { title: "The final empire", id: uuid() },
+  ]);
 
   useEffect(() => {
     setTheme(isLightTheme ? light : dark);
   }, [isLightTheme]);
 
+  const addBook = () => {
+    setBooks([...books, { title: "new book", id: uuid() }]);
+  };
   return (
     <div
       className="book-list"
       style={{ background: theme.bg, color: theme.syntax }}
     >
       <ul>
-        <li style={{ background: theme.ui }}>The way of kings</li>
-        <li style={{ background: theme.ui }}>The name of the wind</li>
-        <li style={{ background: theme.ui }}>The final empire</li>
+        {books.map((book) => (
+          <li key={book.id} style={{ background: theme.ui }}>
+            {book.title}
+          </li>
+        ))}
       </ul>
+      <button onClick={addBook}>Add book</button>
     </div>
   );
 };
