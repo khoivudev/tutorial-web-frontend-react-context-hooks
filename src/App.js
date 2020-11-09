d8e5a8191e246cfbae3a65cee7dd65e1e113d852
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-11-09 21:50:55 +0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Booklist from "./components/Booklist";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Booklist />
+    </div>
+  );
 }
 
 export default App;
